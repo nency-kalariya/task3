26ff10eb59e04cbfc120ed3235a831208ae41696
nency-kalariya <144974985+nency-kalariya@users.noreply.github.com>
2023-10-30 08:17:02 +0530

    Create a PHP script that finds the difference between two arrays using the array_diff() function.
    <?php
    $a1=array("a"=>"Apple","b"=>"Banana","c"=>"Mango","d"=>"Watermelon");
    $a2=array("e"=>"Apple","f"=>"Banana","g"=>"Mango");
    
    $result=array_diff($a1,$a2);
    print_r($result);
    ?>

---
+++ script42.php
@@ -0,0 +1,7 @@
+<?php
+$a1=array("a"=>"Apple","b"=>"Banana","c"=>"Mango","d"=>"Watermelon");
+$a2=array("e"=>"Apple","f"=>"Banana","g"=>"Mango");
+
+$result=array_diff($a1,$a2);
+print_r($result);
+?>
